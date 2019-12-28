02af2817fe2e819eb0c3ae9587bc052922b2ff23
Konstantin Bulygin <korustlt@gmail.com>
2019-12-28 14:22:42 +0400

    deleted hardcoded notes

--- app/src/main/java/com/konstantinbulygin/tasklistapp/MainActivity.java
+++ app/src/main/java/com/konstantinbulygin/tasklistapp/MainActivity.java
@@ -24,17 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
-
-        if (notes.isEmpty()) {
-            notes.add(new Note("BarberShop", "Make hair", "Monday", 2));
-            notes.add(new Note("Shop", "Buy bread", "Tuesday", 2));
-            notes.add(new Note("Work", "Work", "Wednesday", 1));
-            notes.add(new Note("Walk", "Go for a walk", "Friday", 3));
-            notes.add(new Note("Buy food", "Buy a lot of food", "Saturday", 2));
-            notes.add(new Note("Celebrate", "Celebrate the new year", "Sunday", 1));
-            notes.add(new Note("Celebrate", "Celebrate the new year", "Sunday", 1));
-            notes.add(new Note("Celebrate", "Celebrate the new year", "Sunday", 1));
-        }
         final NotesAdapter adapter = new NotesAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
