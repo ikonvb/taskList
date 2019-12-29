eeddda6eba16d4643148082663cb718eacb7ba43
Konstantin Bulygin <korustlt@gmail.com>
2019-12-29 13:08:13 +0400

    work with Room

--- app/src/main/java/com/konstantinbulygin/tasklistapp/AddNewActivity.java
+++ app/src/main/java/com/konstantinbulygin/tasklistapp/AddNewActivity.java
@@ -1,8 +1,6 @@
 package com.konstantinbulygin.tasklistapp;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -20,13 +18,15 @@ public class AddNewActivity extends AppCompatActivity {
     private EditText editTextDescription;
     private Spinner spinnerDaysOfWeek;
     private RadioGroup radioGroupPriority;
-    private NotesDBHelper dbHelper;
-    private SQLiteDatabase database;
+
+    private NotesDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+        database = NotesDatabase.getInstance(this);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -37,8 +37,6 @@ public class AddNewActivity extends AppCompatActivity {
         editTextDescription = findViewById(R.id.editTextDescription);
         spinnerDaysOfWeek = findViewById(R.id.spinnerDaysOfWeek);
         radioGroupPriority = findViewById(R.id.radioGroupPriority);
-        dbHelper = new NotesDBHelper(this);
-        database = dbHelper.getWritableDatabase();
     }
 
     public void onClickSaveNote(View view) {
@@ -50,15 +48,8 @@ public class AddNewActivity extends AppCompatActivity {
         int priority = Integer.parseInt(radioButton.getText().toString());
 
         if (isFilled(title, description)) {
-
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, title);
-            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, description);
-            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, dayOfWeek);
-            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, priority);
-
-            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
-
+            Note note = new Note(title, description, dayOfWeek, priority);
+            database.notesDao().insertNote(note);
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         } else {
--- app/src/main/java/com/konstantinbulygin/tasklistapp/MainActivity.java
+++ app/src/main/java/com/konstantinbulygin/tasklistapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.konstantinbulygin.tasklistapp;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -15,16 +13,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     private RecyclerView recyclerViewNotes;
     private final ArrayList<Note> notes = new ArrayList<>();
     private NotesAdapter adapter;
-    private NotesDBHelper dbHelper;
-    private SQLiteDatabase database;
+    private NotesDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +32,14 @@ public class MainActivity extends AppCompatActivity {
             actionBar.hide();
         }
 
-        dbHelper = new NotesDBHelper(this);
-        database = dbHelper.getWritableDatabase();
-        getData();
+        database = NotesDatabase.getInstance(this);
+
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
         adapter = new NotesAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
+
+        getData();
         adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
 
             @Override
@@ -72,10 +70,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void remove(int position) {
-        int id = notes.get(position).getId();
-        String where = NotesContract.NotesEntry._ID + " = ?";
-        String[] whereArgs = new String[] {Integer.toString(id)};
-        database.delete(NotesContract.NotesEntry.TABLE_NAME, where, whereArgs);
+        Note note = notes.get(position);
+        database.notesDao().deleteNote(note);
         getData();
         adapter.notifyDataSetChanged();
 
@@ -87,24 +83,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getData() {
-
+        List<Note> notesFromDB = database.notesDao().getAllNotes();
         notes.clear();
-
-        String selection = NotesContract.NotesEntry.COLUMN_PRIORITY + " < ?";
-        String[] selectionArgs = new String[] {"2"};
-
-        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, selection, selectionArgs, null, null, NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK);
-
-        while (cursor.moveToNext()) {
-            int id = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry._ID));
-            String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
-            String description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
-            int dayOfWeek = cursor.getInt((cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK)));
-            int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
-            Note note = new Note(id,title, description, dayOfWeek, priority);
-            notes.add(note);
-        }
-
-        cursor.close();
+        notes.addAll(notesFromDB);
     }
 }
